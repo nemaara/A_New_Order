b28e60b400cd88c336443f4291d14c07ab4c0aad
Eric Gallager <egall@gwmail.gwu.edu>
2023-12-31 22:27:52 -0500

    Copy over changes from other repo
    - new linting workflow
    - recompression of images
    - start of work on some lua
    - validator fixes
    - updated README
    see the following issues:
    - #7
    - #32
    - #33, checkbox 2
    - #57 (a bit)
    - #73 (gets started with it at least; I'm holding back on some other parts I've written for it until they're more submittable)
    - #81 (adds FIXMEs about it)
    - #144

--- lua/ano_wml_tags.lua
+++ lua/ano_wml_tags.lua
@@ -1,2 +1,11 @@
 -- (stub file that I may possibly put stuff in in the future)
 
+if not wml_actions then wml_actions = {} end
+
+function wml_actions.get_support_between(cfg)
+	local char1 = cfg.char1
+	local char2 = cfg.char2
+	-- TODO
+	wesnoth.set_variable(ano_support_level, 0)
+end
+
